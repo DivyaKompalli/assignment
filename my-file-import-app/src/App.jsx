9c73f310d14c7123b39d48dbd85422b6d75fb8e6
DivyaKompalli <sridivyakompalli@gmail.com>
2025-02-02 23:48:02 +0530

    second commit

---
+++ my-file-import-app/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import FileImport from "./FileImport.jsx"; // Ensure the import path is correct
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <FileImport />
+    </div>
+  );
+};
+
+export default App;
